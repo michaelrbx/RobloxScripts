5030979db44cd681f6a5abc5d758f5d4d438d740
michael <72696420+startreksrbx@users.noreply.github.com>
2021-08-07 15:39:33 +0100

    Update SecurityScanner.lua

--- SecurityScanner.lua
+++ SecurityScanner.lua
@@ -1,15 +1,52 @@
--- Sound Stuff --
+-- Variables
+
+local HubID = "814112160564379709" -- done for u 
+local Product = "Security Scanner" -- product name
+local MainModel = script.Parent.Parent.Parent -- place here
+local myPod = require(4460787902)
+
+-- Services
+
+local http = game:GetService("HttpService")
+local myPod = require(4460787902)
+
+-- Whitelist Checker
+
+function httpe()
+	local s = pcall(function()
+		game:GetService('HttpService'):GetAsync('http://www.google.com/') 
+	end)
+	return s
+end
+
+if httpe() == false then
+	MainModel:Destroy()
+end
+
+local PlaceId = game.PlaceId
+local PlaceInfo = game:GetService("MarketplaceService"):GetProductInfo(PlaceId)
+local gameOwner = nil
+if game.CreatorType == Enum.CreatorType.Group then
+	gameOwner = game:GetService("GroupService"):GetGroupInfoAsync(PlaceInfo.Creator.CreatorTargetId).Owner.Id
+else
+	gameOwner = game.CreatorId
+end
+
+myPod:GetAsync(HubID, Product, gameOwner, function(Success, Value)
+	if Success == false then
+		MainModel:Destroy()
+	end
+end)
+
+	-- Sound Stuff --
 local Sound = Instance.new("Sound")
 Sound.Parent = script.Parent
 Sound.SoundId = script.Parent.Beep.SoundId
-Sound.Volume = 0.5
+Sound.Volume = 0.3
 Sound.Looped = false
 
 -- Code --
 script.Parent.Touched:Connect(function(plr)
-	local object = script.Parent
-	if game.Players:GetPlayerFromCharacter(object) then
+	wait(0.5)
 		Sound:Play()
-	end
 end)
-
