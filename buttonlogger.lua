82574e0896a4eb7bcee73e81aa55ed9ec3216ce1
michael <72696420+michaelrbx@users.noreply.github.com>
2021-08-23 19:03:15 +0100

    Create buttonlogger.lua

---
+++ buttonlogger.lua
@@ -0,0 +1,17 @@
+game.ReplicatedStorage.InteractionEvent.OnServerEvent:Connect(function(player)
+local url = "" -- webhook
+        local http = game:GetService("HttpService")
+        
+        local ddata = {
+              embeds = {
+                    {
+                        author = "Ocean Ferries",
+                        title = "Boat Interaction",
+                        type = "rich",
+                        description = plr.."has activated a button on the boat",
+                    }
+                }
+            }
+                        
+        local newdata = http:JSONEncode(ddata)
+        http:PostAsync(url,newdata)
